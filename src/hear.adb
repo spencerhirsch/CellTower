08bc933a3db97296c347a6bd4b9959048127995e
s <spencer@yellowcrest.com>
2022-11-07 00:13:09 -0500

    Takes input from the user.

--- src/hear.adb
+++ src/hear.adb
@@ -1,23 +1,36 @@
 with Ada.Text_IO;
+with Ada.Strings;
+with Ada.Integer_Text_IO;
+-- with Ada.Strings.Fixed;
+--with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
-procedure Main is
-    Input : String(1 ... 80);
+procedure Hear is
+    --Input : Unbounded_String;
+    Input : String(1..80);
     Last : Natural;
-    Forever: Boolean;
-    begin
-        Forever = True;
-        while Forever loop
-            Ada.Text_IO.Get_Line(Input, Last)
-            for i in 1 ... Length(Input) loop
-                if Element (Input, i) = '.' then
-                 -- process accordingly
-                elsif Element (Input, i) = '?' then
-                    -- process accordingly
-                else then
-                    Ada.Text_IO.Put_Line("Invalid Input.");
-                    break;
-                
-        end loop;
+    Forever: Boolean := True;
+    -- Cnt : Natural;
+    Last_Index : Integer;
+begin
+    while Forever loop
+        -- Cnt := Ada.Strings.Fixed.Count;
+        Ada.Text_IO.Get_Line(Item => Input, Last => Last);
+        -- Ada.Strings.Fixed.Trim(Source => Input);
+        Last_Index := Input'Length;
+        -- Ada.Integer_Text_IO.Put(Last_Index);
+        if Input(Last_Index - 1) = '.' then
+            -- Add structure to Graph
+            --Ada.Text_IO.Put_Line(Input(Last_Index));
+            Ada.Text_IO.Put_Line(".");
+        elsif Input(Last_Index - 1) = '?' then
+            -- Call function to check for link
+            -- Ada.Text_IO.Put_Line(Input(Last_Index));
+            Ada.Text_IO.Put_Line("?");
+        else
+            Ada.Text_IO.Put_Line("Invalid Input");
+        end if;
+    end loop;
+end Hear;
 
 
             
