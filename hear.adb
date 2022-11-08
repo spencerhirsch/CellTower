547e00689c5e023078d929a3ecab13b62f1d490d
tygutowski <tygutowski@gmail.com>
2022-11-07 21:16:48 -0500

    AJHAHAHAHAHA

--- hear.adb
+++ hear.adb
@@ -1,92 +1,82 @@
 -- Authors: Spencer Hirsch, shirsch2020@my.fit.edu
 --          Tyler Gutowski, tgutowski2020@my.fit.edu
 -- Course:  CSE 4250, Fall 2022
--- Project: Project 3, Can you hear me now?
+-- Project: Project 3, Can you HEAR me now?
 
-with Ada.Text_IO;
-with Ada.Strings;
-with Ada.Integer_Text_IO;
+WITH ADA.TEXT_IO;
+WITH ADA.STRINGS;
+WITH ADA.STRINGS.FIXED;
+WITH ADA.INTEGER_TEXT_IO;
 
-procedure Hear is
-
+PROCEDURE HEAR IS
     -- ########################################################################
-    -- Function to return total number of spaces in a string.
-    function Count (Input : String) return Natural is -- Returns integer [1, inf)
-        Number_Of_Space : Integer := 0;
-    begin
-        for I in Input'Range loop -- For each character in Input
-            if Input(I) = ' ' then -- If its a space, increase word count
-                Number_Of_Space := Number_Of_Space + 1;
-            end if;
-        end loop;
-        return Number_Of_Space; -- Returns number of spaces
-    end Count;
-    
+    -- Does a comment (#) exist in the current line?
+    FUNCTION DOES_COMMENT_EXIST (INPUT: STRING) RETURN BOOLEAN IS
+	BEGIN
+		FOR I IN INPUT'RANGE LOOP
+			IF INPUT(I) = '#' THEN
+				RETURN TRUE;
+			END IF;
+		END LOOP;
+		RETURN FALSE;
+	END DOES_COMMENT_EXIST;
+	
     -- ########################################################################
-    -- Function to return the index of the first . or ?
-    function Get_Index (Input : String) return Natural
-    
+    -- Get the length of a comment (Everything after and including #)
+	FUNCTION GET_COMMENT_LENGTH ( INPUT : STRING ) RETURN INTEGER IS
+	COMMENT_LENGTH : NATURAL := 0;
+	BEFORE_POUND : BOOLEAN := TRUE;
+	BEGIN
+		FOR I IN INPUT'RANGE LOOP
+			IF INPUT(I) = '#' THEN
+				BEFORE_POUND := FALSE;
+			END IF;
+			IF BEFORE_POUND = FALSE THEN
+				COMMENT_LENGTH := COMMENT_LENGTH + 1;
+			END IF;
+		END LOOP;
+		RETURN COMMENT_LENGTH;
+	END GET_COMMENT_LENGTH;
+	
+	FUNCTION GET_COMMAND_LENGTH ( INPUT : STRING; COMMENT_LENGTH : NATURAL ) RETURN INTEGER IS
+	BEGIN
+		RETURN INPUT'LENGTH - COMMENT_LENGTH;
+	END GET_COMMAND_LENGTH;
+	
     -- ########################################################################
-    -- Function to remove all spaces from a string
-    function No_Space (Input : String ; White : Integer) return String is -- Returns a string
-        New_String : String(1..Input'Length - White);
-        Index : Integer := 1;
-    begin
-        for I in Input'Range loop -- For each character in Input
-            if Input(I) /= ' ' then -- If the character is not a space
-                New_String(Index) := Input(I); -- Add the character to New_String
-                Index := Index + 1;
-            end if;
-        end loop;
-        return New_String; -- Returns the string without spaces
-    end No_Space;
-
-    -- ########################################################################
-    -- Function to remove comments from a string.
-    function Fuck_Comments (Input : String ; Comment : Integer) return String is
-        New_String : String(1..Input'Length - Comment) := "";
-        Index : Integer := 1;
-    begin
-        for I in Input'Range loop -- For each character in Input
-            if Input(I) = '#' then
-                ada.Text_IO.Put_Line("THERE IS COMMENT!");
-            end if;
-        end loop;
-        return New_String;
-    end Fuck_Comments;
+    -- Takes String, and removes everything after index
+    FUNCTION REMOVE_AFTER_INDEX (INPUT : STRING; COMMENT_LENGTH : NATURAL) RETURN STRING IS
+		COMMAND_LENGTH : NATURAL := INPUT'LENGTH - COMMENT_LENGTH;
+        NEW_STRING : STRING(1..COMMAND_LENGTH);
+    BEGIN
+		NEW_STRING := INPUT(INPUT'FIRST..COMMAND_LENGTH);
+        RETURN NEW_STRING;
+    END REMOVE_AFTER_INDEX;
 
     -- ########################################################################
-    -- Procedure to process strings
-    procedure String_Process(New_String : String) is
-    Last_Index : Integer := New_String'Length;
-    begin
-        Ada.Text_IO.Put_Line(New_String); -- Prints New_String
-        Ada.Text_IO.Put_Line(New_String((1) .. (5))); -- Prints last 4 characters
-        if New_String(Last_Index - 1) = '.' then
-            -- Add structure to Graph
-            --Ada.Text_IO.Put_Line(New_String(Last_Index));
-            Ada.Text_IO.Put_Line(".");
-        elsif New_String(Last_Index - 1) = '?' then
-            -- Call function to check for link
-            --Ada.Text_IO.Put_Line(New_String(Last_Index));
-            Ada.Text_IO.Put_Line("?");
-        else
-            Ada.Text_IO.Put_Line("Invalid Input");
-        end if;
-    end String_Process;
+    -- Prints strings
+    PROCEDURE PROCESS_STRING(NEW_STRING : STRING) IS
+    BEGIN
+        ADA.TEXT_IO.PUT_LINE(NEW_STRING);
+    END PROCESS_STRING;
 
-    -- ########################################################################
-    -- Loop for user input
-    Input : String(1..80);
-    Last : Natural;
-    Forever: Boolean := True;
-    Spaces : Integer;
-begin
-    while Forever loop
-        Ada.Text_IO.Get_Line(Item => Input, Last => Last); -- Get Input
-        Spaces := Count(Input => Input); -- Count the number of spaces (doesnt really matter)
-        String_Process(Input);
-        --New_String := No_Space(Input => Input, White => Spaces);
-        --String_Process(No_Space(Input => Input, White => Spaces));
-    end loop;
-end Hear;
+LAST : NATURAL;
+FOREVER: BOOLEAN := TRUE;
+ARRAY_SIZE : INTEGER := 150;
+COMMAND_LENGTH : NATURAL;
+COMMENT_LENGTH : NATURAL;
+INPUT : STRING(1..ARRAY_SIZE);
+BEGIN
+    WHILE FOREVER LOOP
+        ADA.TEXT_IO.GET_LINE(ITEM => INPUT, LAST => LAST); -- Get INPUT
+		COMMENT_LENGTH := GET_COMMENT_LENGTH(INPUT);
+		IF DOES_COMMENT_EXIST(INPUT) THEN -- If there is a comment
+			COMMAND_LENGTH := GET_COMMAND_LENGTH(INPUT, COMMENT_LENGTH);
+			ARRAY_SIZE := COMMAND_LENGTH;
+			ADA.TEXT_IO.PUT_LINE(NATURAL'IMAGE(INPUT'LENGTH));
+			INPUT := REMOVE_AFTER_INDEX(INPUT, GET_COMMENT_LENGTH(INPUT)); -- Remove the comment from the end
+		END IF;
+		
+        PROCESS_STRING(INPUT);
+    END LOOP;
+END HEAR;
